c974d930601c3ae426a1697342bb009b165c1ada
Flerp <flpenq@gmail.com>
2025-12-16 04:37:06 -0800

    Added proc event for Improved Shred

---
+++ sql/wip_updates/spell_proc_event.sql
@@ -0,0 +1,17 @@
+-- Improved Shred pwoershift proc
+INSERT INTO spell_proc_event
+(
+    entry,
+    SchoolMask,
+    SpellFamilyName,
+    SpellFamilyMask0,
+    SpellFamilyMask1,
+    SpellFamilyMask2,
+    procFlags,
+    procEx,
+    ppmRate,
+    CustomChance,
+    Cooldown
+)
+VALUES
+(16966, 0, 7, 2147483648, 0, 0, 17408, 524288, 0, 100, 0);
