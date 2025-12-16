61daa044696ff196ad7c937f03aee4d3863fdc20
Flerp <flpenq@gmail.com>
2025-12-16 05:45:18 -0800

    Added cat and bear form heart of the wild proc event
    Weird implementation where hotw on the backend creates a custom spell, linked via a proc event
    Don't wanna implement custom spells to use with spell_learn_spell as don't wanna create possible spell Id conflicts in the future

--- sql/wip_updates/spell_proc_event.sql
+++ sql/wip_updates/spell_proc_event.sql
@@ -33,3 +33,23 @@ INSERT INTO spell_proc_event
 )
 VALUES
 (51409, 0, 7, 0, 0, 0, 1048576, 2, 0, 0, 30);
+
+
+-- Heart of the Wild bear and cat component
+INSERT INTO spell_proc_event
+(
+    entry,
+    SchoolMask,
+    SpellFamilyName,
+    SpellFamilyMask0,
+    SpellFamilyMask1,
+    SpellFamilyMask2,
+    procFlags,
+    procEx,
+    ppmRate,
+    CustomChance,
+    Cooldown
+)
+VALUES
+(24899, 0, 7, 3072, 0, 0, 17408, 524288, 0, 0, 0), -- Bear + Dire Bear
+(24900, 0, 7, 2147483648, 0, 0, 17408, 524288, 0, 0, 0); -- Cat
