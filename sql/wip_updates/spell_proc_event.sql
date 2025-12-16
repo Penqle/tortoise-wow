c71853d0c375925f7682b9583da57dec9046e34b
Flerp <flpenq@gmail.com>
2025-12-16 04:53:53 -0800

    Added proc for Feral Adrenaline

--- sql/wip_updates/spell_proc_event.sql
+++ sql/wip_updates/spell_proc_event.sql
@@ -15,3 +15,21 @@ INSERT INTO spell_proc_event
 )
 VALUES
 (16966, 0, 7, 0, 2147483648, 0, 17408, 524288, 0, 100, 0);
+
+-- Feral Adrenaline crit taken proc
+INSERT INTO spell_proc_event
+(
+    entry,
+    SchoolMask,
+    SpellFamilyName,
+    SpellFamilyMask0,
+    SpellFamilyMask1,
+    SpellFamilyMask2,
+    procFlags,
+    procEx,
+    ppmRate,
+    CustomChance,
+    Cooldown
+)
+VALUES
+(51409, 0, 7, 0, 0, 0, 1048576, 2, 0, 0, 30);
