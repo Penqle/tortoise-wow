b74e86073a375b7bdf0f04935d446418a3e73524
Flerp <flpenq@gmail.com>
2025-12-24 01:01:02 -0800

    Added proc event for Purifying flames
    Overrides the spell family mask with the one for Holy Fire

--- sql/wip_updates/spell_proc_event.sql
+++ sql/wip_updates/spell_proc_event.sql
@@ -15,3 +15,21 @@ INSERT INTO spell_proc_event
 )
 VALUES
 (51466, 0, 6, 1215299712, 0, 0, 327680, 2, 0, 0, 0);
+
+-- Purifying Flames proc event
+INSERT INTO spell_proc_event
+(
+    entry,
+    SchoolMask,
+    SpellFamilyName,
+    SpellFamilyMask0,
+    SpellFamilyMask1,
+    SpellFamilyMask2,
+    procFlags,
+    procEx,
+    ppmRate,
+    CustomChance,
+    Cooldown
+)
+VALUES
+(51470, 0, 6, 1048576, 0, 0, 0, 0, 0, 0, 0);
